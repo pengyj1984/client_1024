a9af8bb3210dcad82322dea85da79925d4584e49
pengyj1984 <pengyj1984@126.com>
2020-10-20 00:08:16 +0800

    通信完成

--- client.go
+++ client.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"encoding/json"
 	"errors"
 	"fmt"
-	"os"
 	"time"
 
 	"github.com/gorilla/websocket"
@@ -16,22 +14,17 @@ const (
 )
 
 type CellInfo struct {
-	row int				// 0 ~ MapWidth - 1
-	line int			// 0 ~ MapHeight - 1
-	gold int			// 这个格子的分数
-	reachable int		// 这个格子上可能的人数
-	opportunity int		// 机遇(到达这个位置后金币数比自己到达这个位置后金币数多的人数)
-	risk int			// 风险(到达这个位置后金币数比自己到达这个位置后金币数少的人数)
+	row         int // 0 ~ MapWidth - 1
+	line        int // 0 ~ MapHeight - 1
+	gold        int // 这个格子的分数
+	reachable   int // 这个格子上可能的人数
+	opportunity int // 机遇(到达这个位置后金币数比自己到达这个位置后金币数多的人数)
+	risk        int // 风险(到达这个位置后金币数比自己到达这个位置后金币数少的人数)
 }
 
-type Save struct{
-	Name string
+type Save struct {
+	Name  string
 	Token string
-}
-
-type LoginMsg struct{
-	msgtype int
-	token string
 }
 
 type GameScore struct {
@@ -80,86 +73,102 @@ type PlayerInfo struct {
 func updateFrame(frameData interface{}) {
 	// 捕获处理过程中的异常, 保证不会出现闪退
 	defer func() {
-		if r := recover(); r != nil{
+		if r := recover(); r != nil {
 			fmt.Println(time.Now(), "捕获到异常: ", r)
 		}
 	}()
 
+	fmt.Println(frameData)
+}
 
+func sendMessage(data interface{}) error {
+	if ws != nil {
+		return ws.WriteJSON(data)
+	}
+	return errors.New("ws is nil")
+}
+
+func recvMessage(data interface{}) (err error) {
+	if ws != nil {
+		return ws.ReadJSON(data)
+	} else {
+		return errors.New("ws == nil")
+	}
+}
+
+func login(uri, token string) error {
+	if ws != nil {
+		ws.Close()
+	}
+
+	var err error
+	ws, _, err = websocket.DefaultDialer.Dial(uri, nil)
+	if err != nil {
+		fmt.Println("websocket连接出错, err = ", err)
+		time.Sleep(time.Second)
+		return err
+	}
+
+	loginMsg := Msg{}
+	loginMsg.Msgtype = 0
+	loginMsg.Token = token
+	err = sendMessage(loginMsg)
+	if err != nil {
+		fmt.Println("Send error while login msg, err = ", err)
+		time.Sleep(time.Second)
+		return err
+	}
+
+	for {
+		time.Sleep(time.Millisecond * 10)
+		data := Msg{}
+		err = recvMessage(&data)
+		if err == nil {
+			if data.Msgtype == -1 {
+				time.Sleep(time.Second)
+				fmt.Println("服务器拒绝, 登录失败")
+				return errors.New("login failed")
+			}
+			fmt.Println("登录成功, msg = ", data)
+			return nil
+		} else {
+			return err
+		}
+	}
 }
 
 var ws *websocket.Conn
 
-func sendMessage(data interface{}){
-	if ws != nil{
-		ws.WriteJSON(data)
-	}
-}
-
-func recvMessage()(data interface{}, err error){
-	if ws != nil{
-		var data interface{}
-		err := ws.ReadJSON(data)
-		return data, err
-	}else{
-		return nil, errors.New("ws == nil")
-	}
-}
-
 func main() {
 	//online
-    // uri := "ws://pgame.51wnl-cq.com:8881/ws"
-    //local debug
-    uri := "ws://localhost:8881/ws"
-	LOGIN:
-	var save Save
-	file, err := os.Open("./save.txt")
-	defer file.Close()
+	uri := "ws://pgame.51wnl-cq.com:8881/ws"
+	//local debug
+	//uri := "ws://localhost:8881/ws"
+	token := "追光骑士团1"
+LOGIN:
+	err := login(uri, token)
 	if err != nil {
-		fmt.Println(time.Now(), "找不到save.txt, 新建一个")
-		file, err = os.Create("./save.txt")
-		str := `{"Name":"追光骑士团", "Token":""}`
-		json.Unmarshal([]byte(str), &save)
-		fmt.Println(save)
-		file.Write([]byte(str))
-	}else{
-		bytes := make([]byte, 128)
-		var count int
-		count, err = file.Read(bytes)
-		data := bytes[0:count]
-		json.Unmarshal(data, &save)
-		fmt.Println(save.Name + ", " + save.Token)
-	}
-
-	ws, _, err = websocket.DefaultDialer.Dial(uri, nil)
-	if err != nil{
-		fmt.Println("websocket连接出错, err = ", err)
-		time.Sleep(time.Second * 2)				// 睡 2 秒再试
 		goto LOGIN
 	}
 	defer ws.Close()
-	loginMsg := LoginMsg{}
-	loginMsg.msgtype = 0
-	loginMsg.token = save.Name
-	sendMessage(loginMsg)
-	
-	for{
-		time.Sleep(time.Millisecond * 10)
-		recv, err = recvMessage()
-		if err == nil{
-			
-		}
-	}
-
 
 	for {
-		time.Sleep(time.Millisecond * 10)				// 睡 10 毫秒
-		var frameData interface{}
-		frameData, err = recvMessage()
-		if err == nil{
-			updateFrame(frameData)
-		}else{
+		time.Sleep(time.Millisecond * 10) // 睡 10 毫秒
+		data := Msg{}
+		err = recvMessage(&data)
+		if err == nil {
+			if data.Msgtype == 1 {
+				fmt.Println("准备")
+				s := Msg{}
+				s.Msgtype = 2
+				s.Token = token
+				sendMessage(s)
+			} else {
+				updateFrame(data)
+			}
+		} else {
 			fmt.Println(err)
+			goto LOGIN
 		}
 	}
 }
