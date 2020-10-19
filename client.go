e31653fdaf9d14c74ed76f13a74cb799e55bf917
pengyinjie <pengyj1984@126.com>
2020-10-19 18:19:40 +0800

    all

---
+++ client.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+const (
+	MapWidth  = 8
+	MapHeight = 6
+)
+
+type CellInfo struct {
+	row int				// 0 ~ MapWidth - 1
+	line int			// 0 ~ MapHeight - 1
+	gold int			// 这个格子的分数
+	reachable int		// 这个格子上可能的人数
+	opportunity int		// 机遇(到达这个位置后金币数比自己到达这个位置后金币数多的人数)
+	risk int			// 风险(到达这个位置后金币数比自己到达这个位置后金币数少的人数)
+}
+
+type Save struct{
+	Name string
+	Token string
+}
+
+func updateFrame(frameData interface{}) {
+	// 捕获处理过程中的异常, 保证不会出现闪退
+	defer func() {
+		if r := recover(); r != nil{
+			fmt.Println(time.Now(), "捕获到异常: ", r)
+		}
+	}()
+
+
+}
+
+var ws *websocket.Conn
+
+func sendMessage(data interface{}){
+	if ws != nil{
+		ws.WriteJSON(data)
+	}
+}
+
+func recvMessage()(data interface{}, err error){
+	if ws != nil{
+		var data interface{}
+		err := ws.ReadJSON(data)
+		return data, err
+	}else{
+		return nil, errors.New("ws == nil")
+	}
+}
+
+func main() {
+	//online
+    // uri := "ws://pgame.51wnl-cq.com:8881/ws"
+    //local debug
+    uri := "ws://localhost:8881/ws"
+	LOGIN:
+	var save Save
+	file, err := os.Open("./save.txt")
+	defer file.Close()
+	if err != nil {
+		fmt.Println(time.Now(), "找不到save.txt, 新建一个")
+		file, err = os.Create("./save.txt")
+		str := `{"Name":"追光骑士团", "Token":""}`
+		json.Unmarshal([]byte(str), &save)
+		fmt.Println(save)
+		file.Write([]byte(str))
+	}else{
+		bytes := make([]byte, 128)
+		var count int
+		count, err = file.Read(bytes)
+		data := bytes[0:count]
+		json.Unmarshal(data, &save)
+		fmt.Println(save.Name + ", " + save.Token)
+	}
+
+	ws, _, err = websocket.DefaultDialer.Dial(uri, nil)
+	if err != nil{
+		fmt.Println("websocket连接出错, err = ", err)
+		time.Sleep(time.Second * 10)				// 睡 10 秒再试
+		goto LOGIN
+	}
+	defer ws.Close()
+
+	for {
+		time.Sleep(time.Millisecond * 10)				// 睡 10 毫秒
+		var frameData interface{}
+		frameData, err = recvMessage()
+		if err == nil{
+			updateFrame(frameData)
+		}else{
+			fmt.Println(err)
+		}
+	}
+}
